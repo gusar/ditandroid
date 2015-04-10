8d6ea03a0e80035c4c8a931adfc887bf7f515362
Paul <paul.brittain@mydit.ie>
2015-04-10 22:04:26 +0100

    added working try-catch to timetable launch code

--- app/src/main/java/ie/dit/android/dit/TimetablesActivity.java
+++ app/src/main/java/ie/dit/android/dit/TimetablesActivity.java
@@ -2,7 +2,24 @@ package ie.dit.android.dit;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
 
 public class TimetablesActivity extends Activity {
+
+    public void execute() {
+        Intent timeIntent = new Intent(Intent.ACTION_VIEW);
+
+        PackageManager packageManager = context. getPackageManager().getInstalledPackages(0);
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(timeIntent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        if (isIntentSafe) {
+            startActivity(timeIntent);
+        }
+    }
 
 }
