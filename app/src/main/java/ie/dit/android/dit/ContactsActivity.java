029a64437eb3dba4bc96c8b6d84fcac18a1d93d9
kennanseno <k_seno@yahoo.com>
2015-04-13 23:21:12 +0100

    Toolbar added in Contacts Section

--- app/src/main/java/ie/dit/android/dit/ContactsActivity.java
+++ app/src/main/java/ie/dit/android/dit/ContactsActivity.java
@@ -25,6 +25,7 @@ public class ContactsActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
+        activateToolbar();
         setupWindowAnimations();
 
 
