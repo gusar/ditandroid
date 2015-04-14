41be30779830fde612aa6b86d94da33c8549f865
Paul <paul.brittain@mydit.ie>
2015-04-14 12:05:17 +0100

    calculator correctly launches for timetable

--- app/src/main/java/ie/dit/android/dit/MainActivity.java
+++ app/src/main/java/ie/dit/android/dit/MainActivity.java
@@ -4,9 +4,11 @@ import android.annotation.TargetApi;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.transition.Slide;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -122,9 +124,31 @@ public class MainActivity extends BaseActivity {
         timetables.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent intent = new Intent();
-               intent.setClassName("com.android.calculator2","com.android.calculator2.Calculator");
-               startActivity(intent);
+                final String packageName = "com.android.calculator2";
+                final String packageActivityName = "com.android.calculator2.Calculator";
+                final String LOG_TAG = MainActivity.class.getSimpleName();
+                Intent intent = new Intent();
+               //intent.setClassName("com.android.calculator2","com.android.calculator2.Calculator");
+                intent.setClassName(packageName,packageActivityName);
+                intent.setAction("foo");
+                try {
+                    if (intent != null && intent.getAction() != null) {
+                        // Activity found.
+                        Log.v(LOG_TAG, "Activity Found");
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    } else {
+                        // Bring user to Play or choose an application.
+                        Log.v(LOG_TAG, "Activity Not Found");
+                        intent = new Intent(Intent.ACTION_VIEW);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.setData(Uri.parse("market://details?id=" + packageName));
+                        startActivity(intent);
+                    }
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Failed to launch: " + e);
+                }
+               //startActivity(intent);
           }
         });
     }
